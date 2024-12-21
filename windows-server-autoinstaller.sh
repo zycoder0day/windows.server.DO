39fd6469914de64ff47ea3471b2833194360fa16
Asfahul Muhib <emuhib@ymail.com>
2024-12-21 13:17:29 +0700

    Update windows-server-autoinstaller.sh

--- windows-server-autoinstaller.sh
+++ windows-server-autoinstaller.sh
@@ -49,19 +49,19 @@ case $choice in
     4)
         # Windows 10
         img_file="windows10.img"
-        iso_link="http://157.245.197.117/WIN10.ISO"
+        iso_link="http://138.197.29.186/WIN10.ISO"
         iso_file="windows10.iso"
         ;;
     5)
         # Windows 11
         img_file="windows11.img"
-        iso_link="http://157.245.197.117/WIN11.ISO"
+        iso_link="http://138.197.29.186/WIN11.ISO"
         iso_file="windows11.iso"
         ;;
     6)
         # Windows 1021h2
         img_file="windows1021h2.img"
-        iso_link="http://157.245.197.117/win1021H2.img"
+        iso_link="http://138.197.29.186/win1021H2.img"
         iso_file="windows1021h2.iso"
         ;;
     *)
